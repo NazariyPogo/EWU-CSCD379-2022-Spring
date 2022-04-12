11a2fbd1019b94d536601c6d52d5568485e30dfa
NazariyPogo <nazariypogo@gmail.com>
2022-04-11 20:31:51 -0700

    Fixed return button

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -5,14 +5,17 @@
       <blockquote class="blockquote">
         &#8220;Wordless&#8221;
         <footer>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Suspendisse dolor ex, lobortis id dapibus feugiat, luctus 
-                at ipsum. Nullam lacus tortor, malesuada eget urna id, 
-                posuere vulputate mi. Proin ut aliquet risus. Cras 
-                fermentum in mauris ac ornare. Etiam aliquam lacus quam, 
-                eu venenatis risus rutrum ac. Nulla facilisi. Nulla 
-                facilisi. Mauris ac ullamcorper ex.</p>
-          <v-btn color="primary" nuxt to="/index">Back to index</v-btn>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Suspendisse dolor ex, lobortis id dapibus feugiat, luctus 
+            at ipsum. Nullam lacus tortor, malesuada eget urna id, 
+            posuere vulputate mi. Proin ut aliquet risus. Cras 
+            fermentum in mauris ac ornare. Etiam aliquam lacus quam, 
+            eu venenatis risus rutrum ac. Nulla facilisi. Nulla 
+            facilisi. Mauris ac ullamcorper ex.
+          </p>
+          <div>
+            <v-btn color="primary" nuxt to="/">Back to index</v-btn>
+          </div>
         </footer>
       </blockquote>
     </v-col>
