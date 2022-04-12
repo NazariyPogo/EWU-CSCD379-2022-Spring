4e17586b25942f5faff5496ce5879d9b9637ba59
NazariyPogo <nazariypogo@gmail.com>
2022-04-11 18:32:38 -0700

    Connected index and game pages

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,5 +1,12 @@
 <template>
-    <v-card>
-        Wordless
-    </v-card>
+    <v-row>
+        <v-card>
+
+        </v-card>
+        <v-btn color="primary" nuxt to="/index">Return to index</v-btn>
+    </v-row>
 </template>
+
+<script lang="ts">
+
+</script>
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -73,7 +73,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Start Game! </v-btn>
+          <v-btn color="primary" nuxt to="/game"> Start Game! </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
