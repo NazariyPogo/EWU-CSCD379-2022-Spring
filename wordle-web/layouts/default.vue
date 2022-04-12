95968c2c1763d1c074aa7c72b46ddc855bf20aaf
NazariyPogo <nazariypogo@gmail.com>
2022-04-11 21:15:17 -0700

    Tiny name improvements

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Gamer Time',
+          to: '/game',
+        },
       ],
       miniVariant: false,
       right: true,
--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -23,6 +23,6 @@
 
 <script>
 export default {
-  name: 'InspirePage',
+  name: 'GamePage',
 }
 </script>
