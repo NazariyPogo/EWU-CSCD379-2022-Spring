56b18015b389a51d83019215cd65e20960150375
NazariyPogo <nazariypogo@gmail.com>
2022-04-26 14:21:55 -0700

    Created about page

---
+++ wordle-web/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card>
+        <v-card-text>
+            This webapp allows the user to play the hit game
+            Wordle in a safe space.<br><br>
+            Also Meg is a great instructor and taught a lot.
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+
+</script>
