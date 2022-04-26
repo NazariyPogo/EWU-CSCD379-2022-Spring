f085f1dc858ff9033ff362755b7564815b1290ef
NazariyPogo <nazariypogo@gmail.com>
2022-04-26 14:40:46 -0700

    Added back button to about page

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -3,11 +3,16 @@
         <v-card-text>
             This webapp allows the user to play the hit game
             Wordle in a safe space.<br><br>
-            Also Meg is a great instructor and taught a lot.
+            Also, Meg is a great instructor and taught a lot.
         </v-card-text>
+        <v-card-actions>
+            <v-btn @click="$router.go(-1)">
+                Back
+            </v-btn>
+        </v-card-actions>
     </v-card>
 </template>
 
-<script>
+<script lang="ts">
 
 </script>
