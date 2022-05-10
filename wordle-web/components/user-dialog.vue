cdb22d2158b83239911d2550ef7be31b62ce0524
PlantBrush <103079343+PlantBrush@users.noreply.github.com>
2022-05-10 11:29:27 -0700

    Added local storage of name

--- wordle-web/components/user-dialog.vue
+++ wordle-web/components/user-dialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class>
     <v-container @click="toggleDialog">
-      User
+      {{newName}}
     </v-container>
 
     <v-dialog v-model="dialog" width="450">
@@ -10,16 +10,10 @@
           <v-card-title>User</v-card-title>
 
           <v-card-text>
-            <v-text-field v-model="name" label="User">
-            </v-text-field>
-
-            <v-menu offset-y>
-              <template #activator="{ on, attrs }">
-                <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                  Submit
-                </v-btn>
-              </template>
-            </v-menu>
+            <v-text-field v-model="name" label="User" />
+            <v-btn color="primary" @click="changeName" dark v-bind="attrs" v-on="on">
+              Save
+            </v-btn>
           </v-card-text>
         </v-container>
       </v-card>
@@ -29,71 +23,21 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import { colors } from 'vuetify/lib'
 
 @Component({})
 export default class SettingsDialog extends Vue {
   dialog = false
-  defaultDarkTheme = this.$vuetify.theme.themes.dark
-  defaultLightTheme = this.$vuetify.theme.themes.light
+  name = "Guest"
+  newName = localStorage.getItem("newName")
 
   toggleDialog() {
     this.dialog = !this.dialog
   }
 
-  turnOnTheLights() {
-    this.$vuetify.theme.dark = false
+  changeName() {
+    localStorage.setItem("newName", this.name)
+    this.newName = localStorage.getItem("newName")
   }
 
-  turnOffTheLights() {
-    this.$vuetify.theme.dark = true
-  }
-
-  purpleTheme() {
-    const purpleTheme = {
-      primary: colors.deepPurple,
-      accent: colors.purple.accent3,
-      secondary: colors.purple,
-      info: '#03A9F4',
-      warning: colors.pink.accent1,
-      error: colors.pink.accent3,
-      success: colors.deepPurple.lighten4,
-    }
-    this.$vuetify.theme.themes.dark = purpleTheme
-    this.$vuetify.theme.themes.light = purpleTheme
-  }
-
-  orangeTheme() {
-    const orangeTheme = {
-      primary: colors.deepOrange,
-      accent: colors.orange.accent3,
-      secondary: colors.brown,
-      info: '#FFA500',
-      warning: colors.yellow.accent1,
-      error: colors.amber.accent3,
-      success: colors.pink.lighten4,
-    }
-    this.$vuetify.theme.themes.dark = orangeTheme
-    this.$vuetify.theme.themes.light = orangeTheme
-  }
-
-  naturalTheme() {
-    const naturalTheme = {
-      primary: colors.lightGreen,
-      accent: colors.brown.base,
-      secondary: colors.blueGrey,
-      info: '#FFA500',
-      warning: colors.yellow.accent1,
-      error: colors.red.accent3,
-      success: colors.lightBlue.lighten4,
-    }
-    this.$vuetify.theme.themes.dark = naturalTheme
-    this.$vuetify.theme.themes.light = naturalTheme
-  }
-
-  defaultTheme() {
-    this.$vuetify.theme.themes.dark = this.defaultDarkTheme
-    this.$vuetify.theme.themes.light = this.defaultLightTheme
-  }
 }
 </script>
