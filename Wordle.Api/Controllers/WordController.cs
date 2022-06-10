ba078fb8cad11c47693c76943cc166e875340c9c
NazariyPogo <nazariypogo@gmail.com>
2022-06-10 15:25:38 -0700

    Initialized WordController/Service no implementation yet

---
+++ Wordle.Api/Controllers/WordController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wordle.Api.Data;
+using Wordle.Api.Services;
+
+namespace Wordle.Api.Controllers
+{
+    public class WordController : Controller
+    {
+        private readonly WordService _service;
+
+        public WordController(WordService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Word> GetWords()
+        {
+            return _service.GetWords();
+        }
+
+        [HttpPost]
+        public void AddWord(string word)
+        {
+            _service.AddWord(word);
+        }
+
+        [HttpDelete]
+        public void DeleteWord(string word)
+        {
+            _service.DeleteWord(word);
+        }
+
+        [HttpPost]
+        public void ChangeCommonFlag(string word)
+        {
+            _service.ChangeFlag(word);
+        }
+    }
+}
---
+++ Wordle.Api/Services/WordService.cs
@@ -0,0 +1,34 @@
+﻿using Wordle.Api.Data;
+
+namespace Wordle.Api.Services
+{
+    public class WordService
+    {
+        private readonly AppDbContext _context;
+
+        public WordService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        internal void AddWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<Word> GetWords()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ChangeFlag(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
